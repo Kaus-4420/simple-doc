e76ea13aefb8fbcc40ad16bb14c455a4ef0c013b
fkarakas <fkarakas.malt@tf1.fr>
2026-02-25 16:09:38 +0100

    fix link in md

--- internal/markdown/markdown.go
+++ internal/markdown/markdown.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer/html"
 )
 
@@ -13,6 +14,9 @@ var md goldmark.Markdown
 func init() {
 	md = goldmark.New(
 		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
 		goldmark.WithRendererOptions(
 			html.WithUnsafe(),
 		),
